89d139e6a06ccec0a9a17ab868cbbcd859b99376
Riina <riina.e.aaltonen@gmail.com>
2026-01-22 15:28:21 +0100

    Konfiguration av viteprojekt

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                images: resolve(__dirname, "images.html"),
+                about: resolve(__dirname, "about.html")
+            }
+        }
+    },
+    plugins: [
+        ViteImageOptimizer({
+            png: {
+                quality: 75
+            }, 
+            jpg: {
+                quality: 75
+            }, 
+            jpeg: {
+                quality: 75
+            },
+            webp: {
+                quality: 70
+            },
+            avif: {
+                quality: 60
+            }
+        })
+    ]
+});
